e512c213f3849f78b0cf3e815213952c48592132
David Walker <dpw@cs.princeton.edu>
2020-04-06 13:28:27 -0400

    main file set up to parse

--- src/bin/Main.ml
+++ src/bin/Main.ml
@@ -1,1 +1,24 @@
-let () = print_endline Dpt.message
+open Batteries
+
+(* the name of the file *)
+let filename = Sys.argv.(1)
+
+let main () =
+  let input = open_in filename in
+  let filebuf = Lexing.from_input input in
+  try
+    print_endline "Parsing ...";
+    let ds = Parser.main Lexer.token filebuf in
+    print_endline "Interpreting...";
+    let _ = Interp.inter_decls ds in
+    print_endline "Done";
+  with
+  | Lexer.Error msg ->
+      Printf.eprintf "%s%!" msg
+  | Parser.Error ->
+      Printf.eprintf "At offset %d: syntax error.\n%!" (Lexing.lexeme_start filebuf)
+  ;
+  IO.close_in input
+
+let _ = main ()
+
